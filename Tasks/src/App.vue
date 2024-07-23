4b6598a04320d32452f514dd9e6022ca8413e430
Rahul Kumar 🚀 <rahulkumarpahwa@gmail.com>
2024-07-24 03:21:44 +0530

    created footer

--- Tasks/src/App.vue
+++ Tasks/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import HeaderComponent from './components/HeaderComponent.vue'
+import FooterComponent from './components/FooterComponent.vue'
 </script>
 
 <template>
   <HeaderComponent />
   <RouterView />
+  <FooterComponent />
 </template>
---
+++ Tasks/src/components/FooterComponent.vue
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<template>
+  <div class="bg-[#41b883] p-8 flex justify-evenly items-center">
+    <a href="https://linkedin.com/in/rahulkumarpahwa" class="text-xl font-mono font-bold"> RahulKumar &copy; 2024-25</a>
+    <div class="w-20 flex gap-5">
+      <a href="https://github.com/rahulkumarpahwa"><img src="https://img.icons8.com/?size=100&id=62856&format=png&color=000000" alt="" /></a>
+       <a href="https://linkedin.com/in/rahulkumarpahwa"><img src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000" alt="" /></a>
+    </div>
+  </div>
+</template>
